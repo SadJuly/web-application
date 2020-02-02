4bc4b8561eb2216e7e1e8d23d1a222d0c16616db
hatkoElie <elie.hatko@gmail.com>
2020-02-03 00:27:24 +0200

    70% of work done

---
+++ src/main/java/com/example/servingwebcontent/Form.java
@@ -0,0 +1,15 @@
+package com.example.servingwebcontent;
+
+public class Form {
+
+  private String content;
+
+  public String getContent() {
+    return content;
+  }
+
+  public void setContent(String content) {
+    this.content = content;
+  }
+
+}
---
+++ src/main/java/com/example/servingwebcontent/WebController.java
@@ -0,0 +1,27 @@
+package com.example.servingwebcontent;
+
+import java.io.IOException;
+import com.google.zxing.WriterException;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class WebController {
+
+	@GetMapping("/qr")
+	public String qrPage(Model model){
+		model.addAttribute("qr", new Form());
+		return "qr";
+	}
+
+	@PostMapping("/qr")
+	public String submitQrPage(@ModelAttribute Form form){
+		GenerateQRCode qr = new GenerateQRCode();
+		qr.generate(form.getContent());
+		return "result";
+	}
+}
