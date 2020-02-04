746ab2e6fe6c3be192e762a490d8fc4c583c83aa
hatkoElie <elie.hatko@gmail.com>
2020-02-05 00:24:25 +0200

    change map

--- src/main/resources/static/js/map.js
+++ src/main/resources/static/js/map.js
@@ -5,13 +5,13 @@ var mymap = L.map('leaflet-map-main', {
     //... other options
 }).setView([-1.405, 20.09], 2);
 
-// mymap.setMaxBounds(mymap.getBounds());
+mymap.setMaxBounds(mymap.getBounds());
 
 L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=' + token, {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
     minZoom: 2,
-    id: 'mapbox/dark-v10',
+    id: 'mapbox/light-v10', //dark-v10
     accessToken: token
 }).addTo(mymap);
 
